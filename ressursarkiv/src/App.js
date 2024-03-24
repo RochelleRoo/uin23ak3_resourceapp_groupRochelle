6fc6e4e5176ca962a0b1c7b2c41475c7f8b8b754
Roo <rochellegopalan@gmail.com>
2024-03-24 22:12:01 +0100

    fix css

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import MainContent from './components/MainContent';
-import './App.css';
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('HTML');
